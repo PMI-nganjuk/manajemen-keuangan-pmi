b8c67ce73534403f7b876e6264cc71bcb451deac
Bintang Ridwan Pribadi <bintangridwan30@gmail.com>
2025-11-12 23:42:57 +0700

    Menambahkan data seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,57 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+   // data dummy tabel users
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Data default untuk Admin
+        User::create([
+            'nama' => 'Admin Utama',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'), // pastikan nanti diganti di production
+            'role' => User::ROLE_ADMIN,
+            'kategori' => User::KATEGORI_KARYAWAN,
+            'nomer_wa' => '081234567890',
+            'alamat' => 'Kantor Pusat',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Data default untuk Manager Keuangan
+        User::create([
+            'nama' => 'Manager Keuangan',
+            'email' => 'manager.keuangan@example.com',
+            'password' => Hash::make('manager123'),
+            'role' => User::ROLE_MANAGER_KEUANGAN,
+            'kategori' => User::KATEGORI_KARYAWAN,
+            'nomer_wa' => '081298765432',
+            'alamat' => 'Kantor Pusat',
+        ]);
+
+        // Data default untuk Staf Keuangan
+        User::create([
+            'nama' => 'Staf Keuangan',
+            'email' => 'staf.keuangan@example.com',
+            'password' => Hash::make('staf123'),
+            'role' => User::ROLE_STAF_KEUANGAN,
+            'kategori' => User::KATEGORI_KARYAWAN,
+            'nomer_wa' => '081277889900',
+            'alamat' => 'Kantor Cabang',
+        ]);
+
+        // Data default untuk Pegawai biasa
+        User::create([
+            'nama' => 'Pegawai Biasa',
+            'email' => 'pegawai@example.com',
+            'password' => Hash::make('pegawai123'),
+            'role' => User::ROLE_PEGAWAI,
+            'kategori' => User::KATEGORI_KARYAWAN,
+            'nomer_wa' => '081200011122',
+            'alamat' => 'Kantor Cabang',
         ]);
     }
 }
