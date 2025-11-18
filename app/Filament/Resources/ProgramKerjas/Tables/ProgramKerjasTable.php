33e44d023913eb83fe89ef505732e2760993cbea
Epulvis <zuhrisaifuddin405@gmail.com>
2025-11-19 03:35:01 +0700

    Dev : Add Section Program & Organisasi

---
+++ app/Filament/Resources/ProgramKerjas/Tables/ProgramKerjasTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources\ProgramKerjas\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ProgramKerjasTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_program')
+                    ->searchable(),
+                TextColumn::make('keterangan')
+                    ->searchable(),
+                TextColumn::make('id_pegawai')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
